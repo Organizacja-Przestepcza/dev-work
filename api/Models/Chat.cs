649950d458af1f7f2fea6d6f7d7f9df3739d2f28
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-05 19:54:46 +0200

    feat(models): add chat class

--- api/Models/Chat.cs
+++ api/Models/Chat.cs
@@ -2,5 +2,8 @@ namespace api.Models;
 
 public class Chat
 {
-    
+    private string ID { get; set; }
+    private string Name { get; set; }
+    private string[] UserList { get; set; }
+    private string[] AdminList { get; set; }
 }
