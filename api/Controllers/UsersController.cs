962934005e2a83952aa097036160ed474979c430
ptr47 <zdz437@protonmail.com>
2024-09-07 21:29:46 +0200

    feat(controllers): create users controller skeleton

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using api.Context;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers;
+[Route("api/users")]
+public class UsersController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public  ActionResult<IEnumerable<User>> GetUsers()
+    {
+      
+        return Ok();
+    }
+
+    [HttpPost]
+    public ActionResult<IEnumerable<User>> AddUser([FromBody] User user)
+    {
+       
+        return Ok();
+    }
+    
+    [HttpPut]
+    public ActionResult<IEnumerable<User>> UpdateUser([FromBody] User user)
+    {
+       
+        return Ok();
+    }
+}
