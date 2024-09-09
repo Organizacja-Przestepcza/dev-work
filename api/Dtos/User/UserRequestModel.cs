9e3fa6a9beb2aad07095026db48ffb42b6a30634
ptr47 <zdz437@protonmail.com>
2024-09-09 19:48:18 +0200

    fix: bio cant be null
    Signed-off-by: ptr47 <zdz437@protonmail.com>

--- api/Dtos/User/UserRequestModel.cs
+++ api/Dtos/User/UserRequestModel.cs
@@ -5,6 +5,6 @@ public class UserRequestModel
     public string Email { get; set; }
     public string Password { get; set; }
     public string Username { get; set; }
-    public string? Bio { get; set; }
+    public string Bio { get; set; } = string.Empty;
 
 }
