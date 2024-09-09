e33ed3a7ab3de1326414fdff0d838c717de4323a
ptr47 <zdz437@protonmail.com>
2024-09-09 20:08:21 +0200

    feat: post mapper first draft
    Signed-off-by: ptr47 <zdz437@protonmail.com>

--- api/Mappers/PostMappers.cs
+++ api/Mappers/PostMappers.cs
@@ -1,6 +1,21 @@
+using api.Dtos.Post;
+using api.Dtos.User;
+using api.Models;
+
 namespace api.Mappers;
 
 public class PostMappers
 {
-    
+    public PostResponseModel ToPostResponseModel(Post post)
+    {
+        return new PostResponseModel()
+        {
+            Id = post.Id,
+            Content = post.Content,
+            CreatedAt = post.CreatedAt,
+            User = UserMappers.ToUserResponseModel(post.User),
+            //Images = post.Images,
+            PreviousPostId = post.PreviousPostId,
+        };
+    }
 }
