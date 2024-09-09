a94d2f80bc2b33d44ffb8c7d1678cfdf5baecf3f
ptr47 <zdz437@protonmail.com>
2024-09-09 19:52:17 +0200

    feat: user mapper
    Signed-off-by: ptr47 <zdz437@protonmail.com>

--- api/Mappers/UserMappers.cs
+++ api/Mappers/UserMappers.cs
@@ -1,6 +1,28 @@
+using api.Dtos.User;
+using api.Models;
+
 namespace api.Mappers;
 
 public class UserMappers
 {
-    
+    public UserResponseModel ToUserResponseModel(User user)
+    {
+        return new UserResponseModel
+        {
+            Id = user.Id,
+            Email = user.Email,
+            Username = user.Username
+        };
+    }
+
+    public User ToUser(UserRequestModel userRequestModel)
+    {
+        return new User()
+        {
+            Id = new Guid(),
+            Email = userRequestModel.Email,
+            Username = userRequestModel.Username,
+            Bio = userRequestModel.Bio
+        };
+    }
 }
