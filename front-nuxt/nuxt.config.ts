0589c9934cf144efa2925bbfc2061a246bfc4554
ptr47 <zdz437@protonmail.com>
2024-09-11 12:16:30 +0200

    feat: front in nuxt

---
+++ front-nuxt/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
