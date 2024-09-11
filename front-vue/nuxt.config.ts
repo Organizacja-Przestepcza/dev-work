53b9f3d79c7d98dae5bbd04c9f260bb0cb185461
ptr47 <zdz437@protonmail.com>
2024-09-11 12:53:51 +0200

    chore: add tailwind

--- front-vue/nuxt.config.ts
+++ front-vue/nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@primevue/nuxt-module'
-  ],
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
