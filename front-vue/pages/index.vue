fd40994cf76024bcd9f043abdda1f78692456895
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-11 20:31:51 +0200

    style: welcome style improvement

--- front-vue/pages/index.vue
+++ front-vue/pages/index.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="flex flex-col-reverse md:flex-row justify-center items-center w-full md:w-3/4 flex-wrap md:flex-nowrap">
-    <div class="w-full md:w-1/2 px-5">
-      <div class="text-center">
-        <p class="text-2xl">Connect with like-minded people at</p>
-        <h1 class="text-5xl">dev-work</h1>
+  <div class="flex items-center justify-center">
+    <div
+      class="flex flex-col-reverse items-center p-5 md:flex-row w-full md:w-3/4 flex-wrap md:flex-nowrap md:shadow-2xl md:shadow-green-400 md:border-2 rounded border-green-400"
+    >
+      <div class="w-full md:w-1/2 px-5">
+        <div class="text-center py-5">
+          <p class="text-2xl">Connect with like-minded people at</p>
+          <h1 class="text-5xl text-nowrap">dev-work</h1>
+        </div>
+        <div class="flex justify-center gap-5 w-full mx-auto text-nowrap">
+          <Button label="Sing up" severity="success" />
+          <Button label="Sign in" severity="secondary" />
+        </div>
       </div>
-      <div class="flex justify-center gap-5 w-full mx-auto text-nowrap py-10">
-        <Button label="Sing up" severity="success" />
-        <Button label="Sign in" severity="secondary" />
+      <div class="w-full md:w-1/2 p-5">
+        <img
+          class="w-[12rem] mx-auto"
+          src="../assets/img/logov2.svg"
+          alt="Developer working on a laptop"
+        />
       </div>
-    </div>
-    <div class="w-full md:w-1/2 p-5">
-      <img
-        class="w-[20rem] md:w-[40rem] mx-auto"
-        src="../assets/img/developer.svg"
-        alt="Developer working on a laptop"
-      />
     </div>
   </div>
 </template>
-
