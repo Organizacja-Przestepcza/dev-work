ed42be962ee5e652f91099ca590a4e5971d15253
ptr47 <zdz437@protonmail.com>
2024-09-11 13:20:38 +0200

    chore: primevue themes config

--- front-vue/nuxt.config.ts
+++ front-vue/nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primevue/themes/aura";
+
 export default defineNuxtConfig({
-  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+});
