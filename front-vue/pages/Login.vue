9f593fccdcc7c5160179d7b875566dcbf53b3fe1
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-12 17:52:32 +0200

    feat: add login page

---
+++ front-vue/pages/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="flex flex-col gap-2">
+      <label for="username">Username</label>
+      <InputText id="username" type="text" />
+    </div>
+    <div class="flex flex-col gap-2">
+      <label for="password">Password</label>
+      <InputText id="password" type="password" />
+    </div>
+    <div class="flex py-2">
+      <Button label="Login" class="w-full max-w-[17.35rem] mx-auto"></Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
