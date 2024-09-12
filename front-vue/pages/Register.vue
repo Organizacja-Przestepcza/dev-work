d72c07c983e95ea7b535e0290356505c75c7fbe4
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-12 18:53:19 +0200

    feat: add register page

---
+++ front-vue/pages/Register.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "basic",
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-4 items-center">
+
+    <!-- Flex row for form sections -->
+    <div class="flex flex-col md:flex-row gap-4 w-full items-stretch">
+
+      <!-- Left Form Section -->
+      <div class="flex flex-col w-full md:w-1/2 gap-4 flex-grow">
+        <div class="flex flex-col gap-2">
+          <label for="username">Username</label>
+          <InputText id="username" type="text" />
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="email">Email</label>
+          <InputText id="email" type="text" />
+        </div>
+      </div>
+
+      <!-- Divider -->
+      <div class="hidden md:flex items-stretch">
+        <Divider layout="vertical" class="h-full"></Divider>
+      </div>
+
+      <!-- Right Form Section -->
+      <div class="flex flex-col w-full md:w-1/2 gap-4 flex-grow">
+        <div class="flex flex-col gap-2">
+          <label for="password">Password</label>
+          <InputText id="password" type="password" />
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="repassword">Re-enter password</label>
+          <InputText id="repassword" type="password" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Register Button -->
+    <div class="w-full md:w-1/2 pt-2">
+      <Button label="Register" class="w-full mx-auto"></Button>
+    </div>
+  </div>
+</template>
--- front-vue/pages/index.vue
+++ front-vue/pages/index.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'basic'
-})
+  layout: "basic",
+});
 </script>
 
 <template>
-    <div class="w-full md:w-1/2 px-5">
-      <div class="text-center py-5 md:pt-10 ">
-        <p class="text-xl md:text-2xl">Connect with like-minded people at</p>
-        <h1 class="text-3xl md:text-5xl">dev-work</h1>
-      </div>
-      <div class="flex justify-center pb-5 md:pb-10 gap-3 md:gap-5 w-full mx-auto">
+  <div class="w-full md:w-1/2 px-5">
+    <div class="text-center py-5 md:pt-10">
+      <p class="text-xl md:text-2xl">Connect with like-minded people at</p>
+      <h1 class="text-3xl md:text-5xl">dev-work</h1>
+    </div>
+    <div
+      class="flex justify-center pb-5 md:pb-10 gap-3 md:gap-5 w-full mx-auto"
+    >
+      <NuxtLink to="register">
         <Button label="Sign up" severity="success" />
-        <Button label="Sign in" severity="secondary" />
-      </div>
+      </NuxtLink>
+      <Button label="Sign in" severity="secondary" />
     </div>
-    <div class="w-full md:w-1/2 p-5">
-      <img class="w-28 md:w-48 mx-auto" src="../assets/img/logov2.svg" alt="Logo" />
-    </div>
+  </div>
+  <div class="w-full md:w-1/2 p-5">
+    <img
+      class="w-28 md:w-48 mx-auto"
+      src="../assets/img/logov2.svg"
+      alt="Logo"
+    />
+  </div>
 </template>
