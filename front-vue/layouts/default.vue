12daea2be54a00030d083c42a344ced9c4088111
ptr47 <zdz437@protonmail.com>
2024-09-13 14:57:32 +0200

    style: change

--- front-vue/layouts/default.vue
+++ front-vue/layouts/default.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="flex"> <!--! need to figure out how to handle mobile -->
-      <!-- Left panel (for menu)-->
-      <div class="w-0 md:w-1/5 p-4 h-screen text-center">
-        This is the left panel.
-      </div>
-  
-      <!-- Main area -->
-      <div class="flex-grow p-4 text-center">
-        <slot/>
-      </div>
-  
-      <!-- Right panel (for friends)-->
-      <div class="w-0 md:w-1/5 p-4 h-screen text-center"> 
-        This is the right panel.
-      </div>
+    <div class="flex">
+        <!-- Left panel (for menu)-->
+        <div class="hidden md:flex w-1/5 p-4 h-screen justify-center">
+            <Button class="h-fit">
+                <Icon name="material-symbols:menu" />
+            </Button>
+        </div>
+
+        <!-- Main area -->
+        <div class="flex-grow p-4 text-center">
+            <slot />
+        </div>
+
+        <!-- Right panel (for friends)-->
+        <div class="hidden w-1/5 p-4 h-screen justify-center">
+            <p>This is the right panel.</p>
+        </div>
     </div>
-  </template>
-  
+</template>
