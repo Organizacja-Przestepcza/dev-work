f30f748a1cd8788d0a093a3f04087fc3eb1a34bb
ptr47 <zdz437@protonmail.com>
2024-09-13 03:25:30 +0200

    feat: default layout skeleton

--- front-vue/layouts/default.vue
+++ front-vue/layouts/default.vue
@@ -1,3 +1,19 @@
 <template>
+    <div class="flex"> <!--! need to figure out how to handle mobile -->
+      <!-- Left panel (for menu)-->
+      <div class="w-0 md:w-1/5 p-4 h-screen text-center">
+        This is the left panel.
+      </div>
   
-</template>
+      <!-- Main area -->
+      <div class="flex-grow p-4 text-center">
+        <slot/>
+      </div>
+  
+      <!-- Right panel (for friends)-->
+      <div class="w-0 md:w-1/5 p-4 h-screen text-center"> 
+        This is the right panel.
+      </div>
+    </div>
+  </template>
+  
