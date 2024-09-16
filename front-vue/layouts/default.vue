193a48ef060d73b59e99f055ef4b6d9a623f9517
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-16 20:14:51 +0200

    refactor: adjust menubar

--- front-vue/layouts/default.vue
+++ front-vue/layouts/default.vue
@@ -1,40 +1,77 @@
 <script setup>
+const notificationCount = ref(3);
+
+const menu = ref();
 const items = ref([
   {
-    label: "Search",
-  },
+    label: 'Manage account',
+    items: [
+      {
+        label: 'View profile',
+        icon: 'pi pi-user',
+        link: '/profile'
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-cog',
+        link: '/settings'
+      },
+      {
+        label: 'Log Out',
+        icon: 'pi pi-sign-out',
+        link: '/logout'
+      }
+    ]
+  }
 ]);
-const notificationCount = ref(3);
+
+const toggle = (event) => {
+  menu.value.toggle(event);
+};
+
 </script>
 
 <template>
-  <div class="card">
-    <Menubar :model="items">
-      <template #start>
+  <!-- Sticky navbar with background -->
+  <div class="sticky top-0 bg-zinc-900 p-3">
+    <div class="flex justify-between items-center">
+      <NuxtLink to="/home">
         <img src="../assets/img/logov3.svg" alt="Logo" width="40" />
-      </template>
+      </NuxtLink>
 
-      <template #item="{ item, props, hasSubmenu, root }">
-        <div class="">
-            <InputText
-                v-if="item.label === 'Search'"
-                placeholder="Search"
-                class="w-64"/>
-        <div class="">
-            <i class="pi pi-bell text-2xl cursor-pointer"></i>
-            <Badge v-if="notificationCount > 0" :value="notificationCount" />
-        </div>
-        
-        </div>
-        
-      </template>
+      <div>
+        <IconField>
+          <InputText v-model="value1" placeholder="Search" />
+          <InputIcon class="pi pi-search" />
+        </IconField>
+      </div>
 
-      <template #end>
+      <div>
         <div class="flex items-center gap-2">
-          Name
-          <Avatar icon="pi pi-user" shape="circle" />
+          <Button severity="secondary">
+            <span class="pi pi-bell" style="font-size: 1rem"></span>
+            <Badge v-if="notificationCount > 0" :value="notificationCount" />
+          </Button>
+          <Avatar icon="pi pi-user" class=" cursor-pointer" shape="circle" @click="toggle" />
+          <Menu ref="menu" id="overlay_menu" :model="items" :popup="true">
+            <template #item="{ item, props }">
+              <NuxtLink :to="item.link" class="flex justify-start gap-2 p-1 pl-3 items-center">
+                <span :class="item.icon"></span>
+                <span>{{ item.label }}</span>
+              </NuxtLink>
+            </template>
+          </Menu>
         </div>
-      </template>
-    </Menubar>
+      </div>
+    </div>
   </div>
+
+  <!-- Content section -->
+  <div class="overflow-y-auto">
+    <slot />
+  </div>
+  <div class="  overflow-y-auto">
+    <slot />
+  </div>
+
 </template>
