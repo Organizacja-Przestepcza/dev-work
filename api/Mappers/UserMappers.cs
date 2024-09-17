22d03097ff75e7b30187e978fc2e9843d44da302
ptr47 <zdz437@protonmail.com>
2024-09-17 17:39:24 +0200

    refactor: removed unused code

--- api/Mappers/UserMappers.cs
+++ api/Mappers/UserMappers.cs
@@ -16,15 +16,6 @@ public static class UserMappers
         };
     }
 
-    public static AppUser ToUser(this UserRequestModel userRequestModel)
-    {
-        return new AppUser()
-        {
-            Email = userRequestModel.Email,
-            UserName = userRequestModel.Username,
-            Bio = userRequestModel.Bio
-        };
-    }
     public static async Task<IdentityResult> SetUserPasswordAsync(UserManager<AppUser> userManager, AppUser appUser, string password)
     {
         var result = await userManager.AddPasswordAsync(appUser, password);
