61d34df06efc96663adc13ac53ffdb9f6ac60af4
ptr47 <zdz437@protonmail.com>
2024-09-17 20:18:58 +0200

    fix: bookmark bug

--- api/Models/Bookmark.cs
+++ api/Models/Bookmark.cs
@@ -1,15 +1,14 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
 
 namespace api.Models;
 
 public class Bookmark
 {
-   [MaxLength(256)] public string Id { get; init; } = null!;
+    [MaxLength(256)] public string Id { get; init; } = null!;
 
-   [MaxLength(256)] public required string PostId { get; set; }
-   public required Post Post { get; set; }
-   [MaxLength(256)] public required string UserId { get; set; }
-   public required AppUser AppUser { get; set; }
-   public DateTime CreatedAt { get; set; }
+    [MaxLength(256)] public required string PostId { get; set; }
+    public Post Post { get; set; }
+    [MaxLength(256)] public required string UserId { get; set; }
+    public AppUser AppUser { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
