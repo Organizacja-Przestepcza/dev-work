3d93ed5f6f3beb6ff9baef4de5eb6422fe81285f
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-18 19:43:00 +0200

    feat: new home layout, moving things around :3

--- front-vue/pages/home.vue
+++ front-vue/pages/home.vue
@@ -1,6 +1,7 @@
-<script setup>
+<script setup lang="ts">
 
 const viewport = useViewport()
+
 watch(viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
     if (viewport.isLessThan('tablet')) {
         setPageLayout('mobile');
@@ -8,43 +9,37 @@ watch(viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
     else {
         setPageLayout('default');
     }
-})
+});
+
+
 </script>
 <template>
     <div class="flex w-5/6 mx-auto gap-5 py-5">
-        <div class="flex flex-wrap gap-5 w-1/5 ">
-
-            <Card>
-                <template #title>Simple Card</template>
-                <template #content>
-                    <p class="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
-                        repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione
-                        quam perferendis esse, cupiditate neque
-                        quas!
-                    </p>
-                </template>
-            </Card>
-            <Card>
-                <template #title>Simple Card</template>
-                <template #content>
-                    <p class="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
-                        repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione
-                        quam perferendis esse, cupiditate neque
-                        quas!
-                    </p>
-                </template>
-            </Card>
-
+        <div class=" w-1/5 ">
+            <PostFilterPanel/>
         </div>
-        <div class="w-3/5 ">
-            <Card style=" overflow: hidden">
-                <template #header>
-                    <img alt="user header" height="5rem" src="../assets/img/logo.svg" />
-                </template>
-                <template #title>Advanced Card</template>
-                <template #subtitle>Card subtitle</template>
+            <div class="w-3/5 ">
+                <Card style=" overflow: hidden">
+                    <template #header>
+                        <img alt="user header" height="5rem" src="../assets/img/logo.svg" />
+                    </template>
+                    <template #title>Advanced Card</template>
+                    <template #subtitle>Card subtitle</template>
+                    <template #content>
+                        <p class="m-0">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
+                            repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa
+                            ratione
+                            quam perferendis esse, cupiditate neque
+                            quas!
+                        </p>
+                    </template>
+                </Card>
+            </div>
+        
+        <div class="w-1/5 flex flex-wrap gap-5 ">
+            <Card>
+                <template #title>Chats</template>
                 <template #content>
                     <p class="m-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
@@ -54,17 +49,18 @@ watch(viewport.breakpoint, (newBreakpoint, oldBreakpoint) => {
                     </p>
                 </template>
             </Card>
-        </div>
-        <div class="w-1/5 ">
-            <Card>
-                <template #title>Simple Card</template>
+            <Card class="w-full">
+                <template #title>Contacts</template>
                 <template #content>
-                    <p class="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
-                        repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione
-                        quam perferendis esse, cupiditate neque
-                        quas!
-                    </p>
+                    <button v-ripple
+                        class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
+                        <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" class="mr-2"
+                            shape="circle" />
+                        <span class="inline-flex flex-col items-start">
+                            <span class="font-bold">Amy Elsner</span>
+                            <span class="text-sm">Admin</span>
+                        </span>
+                    </button>
                 </template>
             </Card>
         </div>
