140b94fa4122d282ea3534ad4a5ac60c8da271b6
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-20 20:08:57 +0200

    feat: create components

---
+++ front-vue/components/ContactTile/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import type { Contact } from '~/common/models';
+
+const props = defineProps({
+  contacts: {
+    type: Object as PropType<Contact[]>,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+  <div v-if="contacts.length>0">
+    <button v-for="contact in contacts"
+    class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
+    <Avatar :image="contact.avatar" class="mr-2" shape="circle" />
+    <span class="inline-flex flex-col items-start">
+      <span class="font-bold">{{ contact.username }}</span>
+    </span>
+  </button>
+  </div>
+  <div v-else>
+    <span>No contacts found.</span>
+  </div>
+</template>
