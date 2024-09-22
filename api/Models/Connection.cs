7748ea9ac0bd796c54a9df326c5758bf7971ecce
ptr47 <zdz437@protonmail.com>
2024-09-23 00:42:03 +0200

    refactor: max length of guids is 38

--- api/Models/Connection.cs
+++ api/Models/Connection.cs
@@ -4,13 +4,13 @@ namespace api.Models;
 
 public class Connection
 {
-    [MaxLength(256)] public string Id { get; set; } = null!;
+    [MaxLength(38)] public string Id { get; set; } = null!;
 
-    [MaxLength(256)] public required string FollowerId { get; set; }
+    [MaxLength(38)] public required string FollowerId { get; set; }
 
     public AppUser Follower { get; set; } = null!;
 
-    [MaxLength(256)] public required string FollowingId { get; set; }
+    [MaxLength(38)] public required string FollowingId { get; set; }
 
     public AppUser Following { get; set; } = null!;
 }
--- api/Models/Message.cs
+++ api/Models/Message.cs
@@ -4,19 +4,19 @@ namespace api.Models;
 
 public class Message
 {
-    [MaxLength(256)] public string Id { get; set; } = null!;
+    [MaxLength(38)] public string Id { get; set; } = null!;
 
-    [MaxLength(256)] public required string UserId { get; set; }
+    [MaxLength(38)] public required string UserId { get; set; }
 
     public AppUser Sender { get; set; }
 
-    [MaxLength(256)] public required string ChatId { get; set; }
+    [MaxLength(38)] public required string ChatId { get; set; }
 
     public Chat Receiver { get; set; }
 
     [MaxLength(1024)] public required string Content { get; set; }
 
-    [MaxLength(256)] public string? ReplyId { get; set; }
+    [MaxLength(38)] public string? ReplyId { get; set; }
 
     public required DateTime SendDate { get; set; }
     public DateTime? ReadDate { get; set; }
