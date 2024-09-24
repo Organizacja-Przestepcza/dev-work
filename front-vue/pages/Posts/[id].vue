4796b66401f6305bf56536c0617bfd8e91e6ed66
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-24 18:09:06 +0200

    feat: add Posts

---
+++ front-vue/pages/Posts/[id].vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { Post } from '~/common/models';
+
+const route = useRoute();
+const runtimeConfig = useRuntimeConfig();
+
+const param = route.params.id;
+const token = useCookie('auth_token').value;
+const { data, status, error, refresh } = await useFetch<Post>(runtimeConfig.public.API_BASE_URL + "/post/" + param, {
+
+    headers: {
+        Authorization: `Bearer ${token}`
+    },
+    lazy: true
+});
+watchEffect(() => {
+    console.log("data: ", data.value);
+    console.log("status: ", status.value);
+    console.log("error: ", error.value);
+    console.log("refresh: ", refresh);
+})
+const toast = useToast();
+const handleToast = (status: string, isBookmarked: boolean) => {
+    if (status == 'error') {
+        toast.add({ severity: 'error', summary: 'Error occured', detail: 'Unable to bookmark this post.', life: 3000 });
+        return;
+    }
+
+    if (isBookmarked) {
+        toast.add({ severity: 'info', summary: 'Added to Bookmarks', detail: 'Post added to bookmarks.', life: 3000 });
+    }
+    else if (!isBookmarked) {
+        toast.add({ severity: 'info', summary: 'Removed from Bookmarks', detail: 'Post removed from bookmarks', life: 3000 });
+    }
+
+}
+
+</script>
+
+<template>
+  <Post @bookmark-click="handleToast" v-if="data" :post="data" />
+  <Comment />
+</template>
