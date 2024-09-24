b8cf7deb3fcba2d52665e201d3622f44121bac78
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-24 18:09:55 +0200

    chore: adjust to dto

--- front-vue/common/models.ts
+++ front-vue/common/models.ts
@@ -1,14 +1,20 @@
-
-export interface Contact {
-    avatar :string,
-    username :string
+export interface User {
+  avatar: string;
+  username: string;
+  displayname?: string
+  bio?: string;
 }
 
 export interface Post {
-    id :string,
-    content :string,
-    createdAt :Date,
-    editedAt? :string,
-    imageUrls? :string[]
-    previousPostId? :string
+  id: string;
+  content: string;
+  createdAt: Date;
+  editedAt?: string;
+  imageUrls?: string[];
+  previousPostId?: string;
+}
+
+export interface Comment {
+  user: User;
+  post: Post;
 }
