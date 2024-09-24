9a481522a1e08aa70d7e7cd73be10d807a17f81b
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-24 18:38:40 +0200

    chore: seed data

--- front-vue/components/Comment/index.vue
+++ front-vue/components/Comment/index.vue
@@ -13,31 +13,47 @@ const comments = ref<Comment[]>([{
         content: 'Comment here',
         createdAt: new Date(),
     }
-}]);
+},
+{
+    user: {
+        avatar: 'https://tr.rbxcdn.com/42eb7f0fcc6028fc0c5a23bc45419d37/420/420/Hat/Webp',
+        username: 'Catissimo Cato',
+        displayname: 'CatBoss69'
+    },
+    post: {
+        id: '1',
+        content: 'Second comment here',
+        createdAt: new Date(),
+    }
+},
+{
+    user: {
+        avatar: 'https://tr.rbxcdn.com/42eb7f0fcc6028fc0c5a23bc45419d37/420/420/Hat/Webp',
+        username: 'Catissimo Cato',
+        displayname: 'CatBoss69'
+    },
+    post: {
+        id: '1',
+        content: 'Third comment here',
+        createdAt: new Date(),
+    }
+}
+]);
 </script>
 <template>
-<div class="flex flex-col gap-5 my-3">
-    <Card v-if="comments && comments.length > 0">
-        <template #content>
+    <div>
+        <div v-if="comments && comments.length > 0" class="flex flex-col gap-5 text-center ">
+
             <div v-for="comment in comments" >
-            <button 
-            class="relative overflow-hidden w-full border-0 bg-transparent flex items-start p-2 pl-4 hover:bg-surface-100 dark:hover:bg-surface-800 rounded-none cursor-pointer transition-colors duration-200">
-            <Avatar :image="comment.user.avatar" class="mr-2" shape="circle" />
-            <span class="inline-flex flex-col items-start">
-                <span class="font-bold">{{ comment.user.username }}</span>
-            </span>
-        </button>
+                <span class="w-full text-gray-500 pb-5 pi pi-ellipsis-v"></span>
+                <Post :post="comment.post" class="w-full"/>
+            </div>
 
-        <div>
-            {{ comment.post.content }}
+
         </div>
-    </div>
-        </template>
-        
-    </Card>    
-    <div v-else>
-        <span>No contacts found.</span>
-    </div>
+        <div v-else>
+            <span>No contacts found.</span>
+        </div>
 
-</div>
+    </div>
 </template>
