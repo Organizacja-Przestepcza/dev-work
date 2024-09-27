0b653d5fb88047d39558d80b103828c5b4923e92
mikemachacky <79257135+mikemachacky@users.noreply.github.com>
2024-09-27 19:46:32 +0200

    chore:eslint config for code quality :)

--- front-vue/eslint.config.js
+++ front-vue/eslint.config.js
@@ -1,61 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 export default [
-  // Pliki, które będą brane pod uwagę
-  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
-
-  // Ustawienia globalne
-  { languageOptions: { globals: globals.browser } },
-
-  // Konfiguracje z @eslint/js (podstawowe zasady JavaScript)
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-
-  // Konfiguracje TypeScript
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsParser,  // parser dla TypeScript
-      parserOptions: {
-        project: './tsconfig.json', // wskaż plik tsconfig.json
-      }
-    }
-  },
-
-  // Konfiguracje Vue
-  pluginVue.configs["flat/essential"],
-
-  // Specjalne ustawienia dla plików Vue z parserem TypeScript
-  {
-    files: ["**/*.vue"],
-    languageOptions: {
-      parserOptions: {
-        parser: tsParser // parser dla TypeScript wewnątrz plików .vue
-      }
-    }
-  },
-
-  // Prettier dla integracji z ESLint
-  prettierConfig,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
   {
     rules: {
-      "prettier/prettier": "error", // Ustawienie błędów dla problemów z Prettier
-    },
-    plugins: {
-      prettier: eslintPluginPrettier,
-    }
-  },
-
-  // Opcjonalnie możesz dodać inne reguły
-  {
-    rules: {
-      "vue/multi-word-component-names": "off",  // Wyłączenie reguły multi-word component names
+      "no-undef": "off",
+      "vue/multi-word-component-names": "off"
     }
   }
 ];
